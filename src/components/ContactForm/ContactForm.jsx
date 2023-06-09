59d17dbfe18792015beb0e9f056e115d38dc80f5
Kateryna Starynska <katerynastarynska@gmail.com>
2023-06-08 22:34:58 -0500

    contactform refactoring

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,80 +1,82 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import css from './ContactForm.module.css';
 
-class ContactForm extends Component {
-  state = {
-    name: '',
-    number: '',
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+
+      case 'number':
+        setNumber(value);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  nameInputId = nanoid();
-  numberInputId = nanoid();
-
-  handleInputChange = e => {
-    const { name, value } = e.currentTarget;
-
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
     const id = nanoid();
     const form = e.currentTarget;
     const name = form.elements.name.value;
     const number = form.elements.number.value;
-
-    this.props.onSubmit({ name, number, id });
-    this.resetForm();
+    console.log(form);
+    onSubmit({ name, number, id });
+    resetForm();
   };
 
-  resetForm = () => {
-    this.setState({
-      name: '',
-      number: '',
-    });
+  const resetForm = () => {
+    setName('');
+    setNumber('');
   };
 
-  render() {
-    const { name, number } = this.state;
-    return (
-      <form className={css.contactForm} onSubmit={this.handleSubmit}>
-        <label className={css.labelForm} htmlFor={this.nameInputId}>
-          Name
-        </label>
-        <input
-          className={css.inpurForm}
-          type="text"
-          name="name"
-          value={name}
-          id={this.nameInputId}
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-          onChange={this.handleInputChange}
-        />
-
-        <label className={css.labelForm} htmlFor={this.numberInputId}>
-          Number
-        </label>
-        <input
-          className={css.inpurForm}
-          type="tel"
-          name="number"
-          value={number}
-          id={this.numberInputId}
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-          onChange={this.handleInputChange}
-        />
-        <button className={css.formBtn} type="submit">
-          Add contact
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form className={css.contactForm} onSubmit={handleSubmit}>
+      <label className={css.labelForm} htmlFor={nameInputId}>
+        Name
+      </label>
+      <input
+        className={css.inpurForm}
+        type="text"
+        name="name"
+        value={name}
+        id={nameInputId}
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+        onChange={handleInputChange}
+      />
+      <label className={css.labelForm} htmlFor={numberInputId}>
+        Number
+      </label>
+      <input
+        className={css.inpurForm}
+        type="tel"
+        name="number"
+        value={number}
+        id={numberInputId}
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+        onChange={handleInputChange}
+      />
+      <button className={css.formBtn} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+};
 
 export default ContactForm;
